0ade9d29386d3852d92f4da4cdcc0d94c33bec98
root <root@techlab360.org>
2014-02-07 10:26:02 -0500

    experimenting with main menu

--- sites/all/themes/storytelling/template.php
+++ sites/all/themes/storytelling/template.php
@@ -1,6 +1,7 @@
 <?php
 
 function storytelling_menu_tree__main_menu($variables) {
+  dpm($variables);
   if (preg_match("/\bexpanded|collapsed\b/i", $variables['tree'])){
     return '<ul id="main-menu-links" class="clearfix">' . $variables['tree'] . '</ul><a href="#" id="menu-toggle">Menu</a>';
   } else {
