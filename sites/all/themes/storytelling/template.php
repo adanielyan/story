f81660c91667e4c7eef9b9e01d62eb9be57c2fde
root <root@techlab360.org>
2014-02-07 13:44:34 -0500

    fixed drop-down menu issue

--- sites/all/themes/storytelling/template.php
+++ sites/all/themes/storytelling/template.php
@@ -1,10 +1,6 @@
 <?php
 
 function storytelling_menu_tree__main_menu($variables) {
-  if (preg_match("/\bexpanded|collapsed\b/i", $variables['tree'])){
-    return '<ul id="main-menu-links" class="clearfix">' . $variables['tree'] . '</ul><a href="#" id="menu-toggle">Menu</a>';
-  } else {
-    return '<ul class="main-menu-sub-links clearfix">' . $variables['tree'] . '</ul>';
-  }
+    return '<ul class="clearfix menu">' . $variables['tree'] . '</ul><a href="#" id="menu-toggle">Menu</a>';
 }
 
